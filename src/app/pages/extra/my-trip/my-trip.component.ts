583d8653f61702a86fb29b550c3dea3ca28e1583
Nguyễn Huỳnh Đông Triều <87523620+trieunhd@users.noreply.github.com>
2024-04-04 21:12:57 +0700

    toast custom

--- src/app/pages/extra/my-trip/my-trip.component.ts
+++ src/app/pages/extra/my-trip/my-trip.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-my-trip',
@@ -8,4 +8,52 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class MyTripComponent {
 
+  toastRef: any;
+
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+
+  showSuccessToast() {
+    this.toastRef = this.toastr.success("The reboot request was unsuccessful", "Device Unreachable", {
+      disableTimeOut: true,
+      tapToDismiss: false,
+      positionClass: 'toast-top-right',
+      toastClass: "toast-icon custom-toast-success"
+  });
+
+
+  }
+
+  showWarningToast() {
+    this.toastRef = this.toastr.warning("The reboot request was unsuccessful", "Device Unreachable", {
+      disableTimeOut: false,
+      tapToDismiss: false,
+      toastClass: "toast-icon custom-toast-warning"
+    });
+  }
+  showInfoToast() {
+    this.toastRef = this.toastr.info("The reboot request was unsuccessful", "Device Unreachable", {
+      disableTimeOut: false,
+      tapToDismiss: false,
+      toastClass: "toast-icon custom-toast-info"
+    });
+  }
+  showErrorToast() {
+    this.toastRef = this.toastr.error("The reboot request was unsuccessful", "Device Unreachable", {
+      disableTimeOut: false,
+      tapToDismiss: false,
+      toastClass: "toast-icon custom-toast-error"
+    });
+
+  }
+
+  removeToast() {
+    this.toastr.clear(this.toastRef.ToastId);
+  }
 }
